a87c80cfcc5d1c3131687001a39a3dbf71f3807d
bnikodym <bnikodym@brentwoodmoschools.org>
2017-07-27 08:23:35 -0500

    team1 update

--- team1.py
+++ team1.py
@@ -5,10 +5,13 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
+import random
+team_name = 'oldfolks' # Only 10 chars displayed.
+strategy_name = 'Collude 60% unless betrayed within last 10 rounds.'
+strategy_description = '''\
+Betray if betrayed in last 10 rounds. otherwise collude 60%of the time
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+'''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -18,15 +21,16 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    if 'b' in their_history[-10:]: # If the other player has betrayed within last 10 rounds, 
+        return 'b'               # Betray.
+    else:
+        if random.random()<0.4: # 10% of the other rounds
+            return 'b'         # Betray
+        else:
+            return 'c'         # but 90% of the time collude
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
