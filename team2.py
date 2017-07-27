204222119226d25afebdf3b14ba9fdf3e20a51ce
scneeman <heyscottn@gmail.com>
2017-07-27 08:18:29 -0500

    TitForTat strategy

--- team2.py
+++ team2.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'TitForTat!' # Only 10 chars displayed.
+strategy_name = 'tit-for-tat'
+strategy_description = 'Mimic the opponents last move'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,19 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    decision = ''
+    if len(my_history) == 0 or len(their_history)==0:
+        decision = 'c'
+    elif 'b' in their_history:
+        decision = 'b'
+   # elif their_history[-1]=='b':
+    #    decision = 'b'
+    elif their_history[-1]=='c':
+        decision = 'c'    
+    else:
+        decision = 'b'
+    
+    return decision
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
