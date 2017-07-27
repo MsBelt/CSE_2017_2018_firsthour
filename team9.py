354d9d7c53690f3577c4da37b200a6088c9a7268
rjuengel <30477974+rjuengel@users.noreply.github.com>
2017-07-27 08:16:12 -0500

    Team 9's game strategy

--- team9.py
+++ team9.py
@@ -1,3 +1,5 @@
+import random
+
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,10 +8,8 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+team_name = 'Team Tina' # Only 10 chars displayed.
+strategy_name = 'Betray at first strike! Retaliation protection and random choice.'
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -26,7 +26,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history)==0:
+        return 'b'
+    elif my_history[-1]=='b' and their_history[-1]=='c':
+        return 'b'
+    else:
+        return random.choice(['b','c'])
+        
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
